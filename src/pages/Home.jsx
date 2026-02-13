c826cb9368af175def670e793a2e2bf87c410601
Hans Feliciano <hgvfeliciano10@gmail.com>
2026-02-12 22:46:47 -0700

    added the calgary map overlay to display to the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,38 @@
+import { useState } from "react";
+import 'leaflet/dist/leaflet.css';
+
 import StationFilter from "../components/StationFIlter";
+import CalgaryMap from "../components/CalgaryMap";
 
 export default function Home() { 
-  const handleFilterChange = (filter) => { 
-    console.log("Filters:", filter)
-    // filters will be used to update station data
+  const [filters, setFilters] = useState({
+    searchQuery: '',
+    category: '',
+    transitLine: 'all'
+  })
+
+  const handleFilterChange = (newFilters) => { 
+    console.log("Filters applied:", newFilters)
+    setFilters(newFilters)
 
   }
 
   return ( 
-    <StationFilter
-      onFilterChange={handleFilterChange}
-      isAuthenticated={false} // set to true to hide the auth alert if user is authenticated
-    />
+    <div className="min-h-screen bg-gray-100 p-5">
+      <div className="flex flex-col lg:flex-row gap-5 max-w-7xl mx-auto h-auto lg:h-[calc(100vh-2.5rem)]">
+        {/* Filter Component */}
+        <div className="w-full lg:w-[360px] h-auto lg:h-full flex-shrink-0">
+          <StationFilter
+            onFilterChange={handleFilterChange}
+            isAuthenticated={false}
+          />
+        </div>
+      
+        {/* Map Panel */}
+        <div className="flex-1 h-[500px] lg:h-full bg-white rounded-lg shadow-lg overflow-hidden">
+          <CalgaryMap filters={filters} />
+        </div>
+      </div> 
+    </div>    
   )
 }
